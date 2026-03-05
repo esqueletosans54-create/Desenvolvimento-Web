61187866815f210d78adacdb701fc93fc0f6d74f
sadsans54 <esqueletosans54@gmail.com>
2026-03-05 11:32:05 -0300

    test2

--- test.java
+++ test.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String [] args) {
-        System.out.println("Esse é meu primero projeto");
-        System.out.println("Cesa KKKKKKKKKKK");
+        Scanner leitor = new Scanner(System.in);
+
+        int number1;
+        int number2;
+        String opera;
+        
+        System.out.print("Digite um numero: ");
+        number1 = leitor.nextInt();
+
+        leitor.nextLine();
+
+        System.out.print("Digite uma operação ");
+        opera = leitor.nextLine();
+
+        System.out.print("Digite um numero: ");
+        number2 = leitor.nextInt();
+
+
+        switch (opera) {
+            case "+" :
+                System.out.println(number1 + number2);
+                break;
+            case "-" :
+                System.out.println(number1 - number2);
+                break;
+            case "*" :
+                System.out.println(number1 * number2);
+                break;
+            case "/" :
+                System.out.println(number1 / number2);
+                break;    
+        }
+
+
     }
 }
