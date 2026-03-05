7e8ec82675b263fc7dc22a2f325d5959c0129f2f
sadsans54 <esqueletosans54@gmail.com>
2026-03-05 11:12:32 -0300

    teste1

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String [] args) {
+        System.out.println("Esse é meu primero projeto");
+        System.out.println("Cesa KKKKKKKKKKK");
+    }
+}
