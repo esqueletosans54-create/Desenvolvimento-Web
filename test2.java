1bd1da6ce66839fd4e6f35e2f9e3ec34c95c8102
sadsans54 <esqueletosans54@gmail.com>
2026-03-06 07:47:30 -0300

    Meu segundo projeto

---
+++ test2.java
@@ -0,0 +1,12 @@
+public class test2 {
+    public static void main(String [] args) {
+
+        for (int i = 0; i < 100; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i +" É par");
+            } else {
+                System.out.println(i + " É inpar");
+            }
+        }
+    }
+}
